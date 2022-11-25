3f32c1225bc7c28f12ae73338e1aff04121133fe
akhil <akhil.paul@ymail.com>
2022-11-25 14:29:10 +0530

    Adds add products feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoute:Routes = [{path:"",component:AddProductsComponent},
+                        {path:"add",component:AddProductsComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
